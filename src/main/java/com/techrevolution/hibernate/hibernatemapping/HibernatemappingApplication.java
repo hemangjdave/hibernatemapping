f7b5d754d8898d77fa3de438683bf65bd407683b
Hemang Dave <hemangjdave@gmail.com>
2021-01-04 22:53:20 +0530

    Adding the first stage of hibernate mappings. this is initial commit to the project.

---
+++ src/main/java/com/techrevolution/hibernate/hibernatemapping/HibernatemappingApplication.java
@@ -0,0 +1,13 @@
+package com.techrevolution.hibernate.hibernatemapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatemappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernatemappingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techrevolution/hibernate/hibernatemapping/HibernatemappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techrevolution.hibernate.hibernatemapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernatemappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
